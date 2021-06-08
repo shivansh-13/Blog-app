d4d747187a422c8025a7d0be4745d1c1d9e53229
shivansh-13 <shivansh01308@gmail.com>
2021-06-09 01:13:33 +0530

    Added ft. to home.js & some styling to in

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,13 @@
- const Navbar = () => {
-     return ( 
-         <nav className = 'navbar'>
-             <h1>Welcome to blogger</h1>
-             <div className='links'>
-                 <a href = '/'>
-                    Home
-                 </a>
-                 <a href = '/create'>
-                    New blog
-                 </a>
-             </div>
-         </nav>
-      );
- }
-  
- export default Navbar;
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <h1>Welcome to blogger</h1>
+            <div className='links'>
+                <a href='/'>Home</a>
+                <a href='/create'> New blog</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;

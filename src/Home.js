afc3502cfab67ae81f339bb4db6390b96e6a621f
shivansh-13 <shivansh01308@gmail.com>
2021-06-09 01:14:13 +0530

    Added ft. to home . js & a bit of styling to it

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 const Home = () => {
+    const [blog, setBlog] = useState([
+        { title: 'My first blog', Body: 'lorem ipsum ...', author: 'Yash', id: '1' },
+        { title: 'My birthday', Body: 'lorem ipsum ...', author: 'Shivansh', id: '2' },
+        { title: 'My first day at college', Body: 'Khula hi nai abhi tak :(', author: 'Shivansh', id: '3' }
+    ]);
+    return (
+        <div className='home'>
+            {blog.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author} </p>
+                    
+                </div>
+            ))}
 
-    const handleClick = (e) => {
-        console.log('hello',e);
-    }
-    return (<div className='home'>
-        <h2>Homepage</h2>
-
-        <button onClick={handleClick}>
-            Click me
-         </button>
-
-    </div>);
+        </div>
+    );
 
 }
 

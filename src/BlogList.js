52c42b6ff853cd61851d12201df9a454660a07a8
shivansh-13 <shivansh01308@gmail.com>
2021-06-10 01:29:02 +0530

    Linked all the files to the db.

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,12 @@
-const BlogList = ({blogProp,handleDelete}) => {
+const BlogList = ({blogProp}) => {
     const blog = blogProp;
     return (
         <div className="blog-list">
             {blog.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
+                    <p>{blog.body}</p>
                     <p>Written by {blog.author} </p>
-                    <button className="button" onClick={() =>handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,20 @@
 import { useState , useEffect} from 'react';
 import BlogList from './BlogList';
 const Home = () => {
-    const [blog, setBlog] = useState([
-        { title: 'My first blog', Body: 'lorem ipsum ...', author: 'Yash', id: '1' },
-        { title: 'My birthday', Body: 'lorem ipsum ...', author: 'Shivansh', id: '2' },
-        { title: 'My first day at college', Body: 'Khula hi nai abhi tak :(', author: 'Shivansh', id: '3' }
-    ]);
-    const handleDelete = (id) => {
-        const newBlogs = blog.filter(blog => blog.id !== id);
-        setBlog (newBlogs);
-    };
+    const [blog, setBlog] = useState(null);
     useEffect(()=>{
-
-        }
+            fetch('http://localhost:3000/blog')
+            .then(res => {
+                return res.json() ;
+            })
+            .then(data =>{
+                setBlog(data);
+            })
+        },[]
     );
     return (
         <div className='home'>
-            <BlogList blogProp={blog} handleDelete={handleDelete} />
+          { blog && <BlogList blogProp={blog}  />}
         </div>
     );
 

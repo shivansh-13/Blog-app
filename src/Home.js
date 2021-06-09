f174f250a242584d426671fccd54095d15a652e8
shivansh-13 <shivansh01308@gmail.com>
2021-06-10 01:03:30 +0530

    Added a data folder with a database in it.

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import BlogList from './BlogList';
 const Home = () => {
     const [blog, setBlog] = useState([
@@ -10,6 +10,10 @@ const Home = () => {
         const newBlogs = blog.filter(blog => blog.id !== id);
         setBlog (newBlogs);
     };
+    useEffect(()=>{
+
+        }
+    );
     return (
         <div className='home'>
             <BlogList blogProp={blog} handleDelete={handleDelete} />
